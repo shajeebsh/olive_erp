24ac23376e4695d88acfacd9166772d671b13948
shajeebs <shajeeb.s@gmail.com>
2026-03-12 22:44:57 +0000

    build: update Django settings and initial migrations
    - Switch database engine from mysql.connector.django to django.db.backends.mysql
    - Update database credentials and name to oliveerp
    - Standardize string formatting in manage.py and settings to use double quotes
    - Add initial migrations for core, home, and dashboard apps to establish database schema

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -6,8 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wagtailerp.settings.base')
-    
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtailerp.settings.base")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,9 +16,9 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    
+
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
